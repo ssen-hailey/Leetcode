139127100efeb410d334792c7d791dbca5972342
Hailey Park <117366758+ssen-hailey@users.noreply.github.com>
2022-11-21 22:37:20 +0900

    Create PalindromNumber.py

---
+++ PalindromeNumber/PalindromNumber.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, x):
+
+        #change data type to str
+        #reverse str
+        #using == to figure out True or False
+
+        return str(x)[::-1]==str(x)
+        
+
+
+        """
+        :type x: int
+        :rtype: bool
+        """
